b2518848be820f2b6071e1c92675fa9e8624117d
VandevilleF <flo.vandeville@gmail.com>
2025-02-05 18:34:46 +0100

    Add some modif in manifest for permission, change color in popup.js and add background.js for managing certain background tasks

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+// Purpose: This file is responsible for changing the background color of all divs on the current tab to #303030 when the button is clicked.
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('changeColor').addEventListener('click', () => {
     chrome.tabs.query({ active: true, currentWindow: true}, (tabs) => {
@@ -12,6 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
 function changeDivBackgrounds() {
   const divs = document.querySelectorAll('div');
   divs.forEach(div => {
-    div.style.backgroundColor = 'red';
+    div.style.backgroundColor = '#303030';
   })
 }
