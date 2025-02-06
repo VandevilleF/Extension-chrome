0ec21dafc3464c3a9ac2a3ae2d48898aa0e5a9f6
VandevilleF <flo.vandeville@gmail.com>
2025-02-06 16:53:38 +0100

    Final version v1, waiting for general code optimization and automatic activation of the extension once the page is reloaded

--- Dark-mod_intranet/popup.js
+++ Dark-mod_intranet/popup.js
@@ -1,6 +1,6 @@
 // Purpose: This file is responsible for changing the background color of all divs on the current tab to #303030 when the button is clicked.
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('changeColor').addEventListener('click', () => {
+  document.getElementById('toggle').addEventListener('click', () => {
     chrome.tabs.query({ active: true, currentWindow: true}, (tabs) => {
       chrome.scripting.executeScript({
         target: { tabId: tabs[0].id },
@@ -23,12 +23,25 @@ function changeDivBackgrounds() {
   const listgroupe = document.querySelectorAll('.list-group-item');
   const precode = document.querySelectorAll('pre');
 
+  // BoxShadow panel
+  const panelsboxshadow = document.querySelectorAll('.panel.panel-default');
+  const panelheaderboxshadow = document.querySelectorAll('.project-header');
+
   // Navbars elements
   const layoutbar = document.getElementById('student-switch-curriculum');
   const sidebar = document.querySelectorAll('.sidebar');
   const logocontainer = document.querySelectorAll('.logo-container');
   const lastitems = document.querySelectorAll('.sidebar > ul > li:last-child');
 
+  // Home page
+  const progressinfo = document.querySelectorAll('.progress-information');
+  const summarycard = document.querySelectorAll('.summary-card');
+  const rectbackground = document.querySelectorAll('.highcharts-background');
+
+  // Modal Content
+  const modalcontent = document.querySelectorAll('.modal-content');
+
+  // Start changing the background color of all elements
   // Body style
   body.style.backgroundColor = '#303030';
   body.style.color ='#fefefe';
@@ -43,6 +56,29 @@ function changeDivBackgrounds() {
   })
   lastitems.forEach(item => {
     item.style.backgroundColor = '#404040';
+  })
+
+  // Change home page
+  progressinfo.forEach(progress => {
+    progress.style.backgroundColor = '#303030';
+  })
+  summarycard.forEach(card => {
+    card.style.backgroundColor = '#303030';
+    card.style.borderColor = 'rgba(254, 254, 254, 0.5)';
+    card.style.boxShadow = '5px 5px 10px 1px rgba(254, 254, 254, 0.1)';
+  })
+  rectbackground.forEach(rect => {
+    rect.setAttribute('fill', '#303030');
+  })
+
+  // Change box shadow of panel
+  panelsboxshadow.forEach(panel => {
+    panel.style.boxShadow = '5px 5px 10px 1px rgba(254, 254, 254, 0.1)';
+    panel.style.backgroundColor = '#303030';
+    panel.style.borderColor = 'rgba(254, 254, 254, 0.5)';
+  })
+  panelheaderboxshadow.forEach(panel => {
+    panel.style.boxShadow = '5px 5px 10px 1px rgba(254, 254, 254, 0.1)';
   })
 
   // Change color of panel project header
@@ -76,4 +112,16 @@ function changeDivBackgrounds() {
     p.style.backgroundColor = '#333333';
     p.style.color = '#fefefe';
   })
+
+  // Modal content
+  modalcontent.forEach(modal => {
+    modal.style.boxShadow = '0 5px 15px rgba(254, 254, 254, 0.5)';
+  })
+  const style = document.createElement('style');
+  style.innerHTML = `
+  .modal-header { background-color: #404040 !important; }
+  .modal-header .close { color: #fefefe !important; }
+  .modal-body { background-color: #303030 !important; }
+  `;
+  document.head.appendChild(style);
 }
